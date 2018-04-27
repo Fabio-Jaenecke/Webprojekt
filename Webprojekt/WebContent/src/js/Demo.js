632bfb33d7750b0bc2304cd49528d1cf27389948
caradrap <caradrap@students.zhaw.ch>
2018-04-27 08:11:29 +0200

    outcommenting of event handler

--- Webprojekt/WebContent/src/js/Demo.js
+++ Webprojekt/WebContent/src/js/Demo.js
@@ -8,6 +8,8 @@ $(function(){
 
     $(".form-inner").append(tasklist.render());
 
+    // TODO either take this event handler or the one in taks.js
+    /*
     $( ".span-4" ).keypress(function(event) {
         var taskId = tasklist.tasks[$(this).attr("taskid")];
         //Id 
@@ -22,22 +24,5 @@ $(function(){
 
         //Current Value
         console.log(event.target.value)
-    });
-
-
-    $("#taskList").on("keydown change", "input",function(event) {
- 
-        var type = $($(this)[0]).attr("type");
-        var current_task = $(this).parent().data("task");
-     alert(current_task);
-        if(type ==="text"){
-          current_task.title =  $(this).val();
-          $(this).parent().data("task",current_task);
-        }else{
-          current_task.done = $(this).is(":checked");
-          $(this).parent().data("task",current_task);
-        }
-     
-        console.log("Dieser Task", $(this).parent().data("task"))
-      });
+    });*/
 });
