f96b05f7009acbb1098342b92b932ac20525e455
caradrap <caradrap@students.zhaw.ch>
2018-05-01 11:42:55 +0200

    added node-modules, cleaned up project structure.

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # Webprojekt
 Webprojekt für WEB2
+Neue Projektstrukur: 
+im src-Folder sind alle javascript-Klassen und Tests, die wir geschrieben haben.
+das index.html und der SpecRunner.html sind jetzt im obersten Verzeichnis.
+zusätzlich habe ich noch die node_modules installiert, so dass wir eine jquery-Autocompletion haben sollten.
+die eclipse-Einstellungen habe ich für den Moment rasugelöscht, so dass die nicht mehr auf dem Repo sind.
---
+++ src/js/TaskList.js
@@ -0,0 +1,29 @@
+let TaskList = function(title){
+    this.title = title;
+    this.tasks = [];
+}
+
+TaskList.prototype.setTitle = function(title, index){
+    this.task[index].title = title;
+}
+
+TaskList.prototype.addTask = function(task) {
+   this.tasks.push(task);
+}
+TaskList.prototype.checkTask = function(index){
+    this.tasks[index].check(); 
+}    
+TaskList.prototype.uncheckTask = function(index){
+    this.tasks[index].uncheck(); 
+}    
+TaskList.prototype.removeTask = function(index){
+    this.tasks.splice(index, 1);
+}
+TaskList.prototype.render = function(){
+    var $markup = $('<ul>');
+    var _i;
+    for (_i = 0; _i < this.tasks.length; _i += 1) {
+      $markup.append(this.tasks[_i].render());
+    }
+    return $markup;
+}
